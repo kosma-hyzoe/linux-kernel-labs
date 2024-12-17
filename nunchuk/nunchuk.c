8183bd62ae14b6f67565cdda246522a0d0184d11
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2024-12-17 22:19:12 +0100

    simple nunchuk driver

--- nunchuk/nunchuk.c
+++ nunchuk/nunchuk.c
@@ -4,15 +4,10 @@
 #include <linux/i2c.h>
 #include <linux/delay.h>
 
-/* i2c_adapter
- *
- *
- *
-
 /* Add your code here */
 #define INIT_BYTES_1	{0xf0, 0x55}
 #define INIT_BYTES_2	{0xfb, 0x00}
-#define INIT_DELAY	10000
+#define INIT_DELAY	1000
 
 
 #define DELAY		10000
@@ -24,89 +19,92 @@ struct nunchuk_dev {
 
 static int _nunchuk_read_regs(struct i2c_client *client, char *rx_buf)
 {
-	int ret_val;
-	int zpressed, cpressed;
+	int ret;
 	char tx_buf[] = {0x00};
 
 	usleep_range(DELAY, DELAY_MAX);
 
-	ret_val = i2c_master_send(client, tx_buf, sizeof(tx_buf));
-	if (ret_val != 1) {
-		pr_alert("i2c_master_send: error %d\n", ret_val);
-		return ret_val;
+	ret = i2c_master_send(client, tx_buf, sizeof(tx_buf));
+	if (ret != 1) {
+		pr_alert("i2c_master_send: error %d\n", ret);
+		return ret;
 	}
 	usleep_range(DELAY, DELAY_MAX);
 
-	ret_val = i2c_master_recv(client, rx_buf, 6);
-	if (ret_val != 1) {
-		pr_alert(" i2c_master_recv: error %d\n", ret_val);
-		return ret_val;
+	ret = i2c_master_recv(client, rx_buf, 6);
+	if (ret != 6) {
+		pr_alert(" i2c_master_recv: error %d\n", ret);
+		return ret;
 	}
-
-	zpressed =
-	cpressed =
 	return 0;
 }
-
-static int _nunchuk_write(struct i2c_cilent *client, char *rx_buf)
-{
-	return 0;
-}
-
 
 static int nunchuk_probe(struct i2c_client *client)
 {
-	int ret_val;
+	int ret, zpressed, cpressed;
 	const char tx_buf1[2] = INIT_BYTES_1;
 	const char tx_buf2[2] = INIT_BYTES_2;
+	u8 recv[6];
 
-	ret_val = i2c_master_send(client, tx_buf1, sizeof(tx_buf1));
-	if (ret_val != 2) {
-		pr_alert("%s: error %d while sending bytes", __func__, ret_val);
-		return ret_val;
+	ret = i2c_master_send(client, tx_buf1, sizeof(tx_buf1));
+	if (ret != 2) {
+		pr_alert("%s: error %d while sending bytes", __func__, ret);
+		return ret;
 	}
 
 	udelay(INIT_DELAY);
 
-	ret_val = i2c_master_send(client, tx_buf2, sizeof(tx_buf2));
-	if (ret_val != 2) {
-		pr_alert("%s: error %d while sending bytes", __func__, ret_val);
-		return ret_val;
+	ret = i2c_master_send(client, tx_buf2, sizeof(tx_buf2));
+	if (ret != 2) {
+		pr_alert("%s: error %d while sending bytes", __func__, ret);
+		return ret;
 	}
 
-	 char rx_buf[6];
-	_nunchuk_read_regs(client, rx_buf);
+	/* dummy read to init the device */
+	ret = _nunchuk_read_regs(client, recv);
+	if (ret < 0)
+		return ret;
+
+	ret = _nunchuk_read_regs(client, recv);
+	if (ret < 0)
+		return ret;
+
+	zpressed = (recv[5] & BIT(0)) ? 0 : 1;
+	if (zpressed)
+		dev_info(&client->dev, "Z button pressed\n");
+
+	cpressed = (recv[5] & BIT(1)) ? 0 : 1;
+	if (cpressed)
+		dev_info(&client->dev, "C button pressed\n");
 
 	return 0;
 }
 
-static void nunchuk_remove(void)
+static void nunchuk_remove(struct i2c_client *client)
 {
-
+	return;
 }
 
-/* TODO: actually of_device_id??? */
-static struct of_device_id nunchuck_match_table[] = {
+// static const struct i2c_device_id nunchuk_device_id[] = {
+// 	{ "nintendo,nunchuk" },
+// 	{ }
+// };
+
+static const struct of_device_id nunchuk_dt_match[] = {
 	{ .compatible = "nintendo,nunchuk" },
-	{ }
+	{ },
 };
+MODULE_DEVICE_TABLE(of, nunchuk_dt_match);
 
-static const struct i2c_device_id nunchuk_device_id[] = {
-	{ "nintendo,nunchuk" },
-	{ }
-};
-/* this generates a modalias string when the device is connected */
-MODULE_DEVICE_TABLE(i2c, nunchuk_device_id);
-
-
-
-struct i2c_driver nunchuk_driver = {
-	.probe = nunchuk_probe,
-	.id_table = nunchuk_device_id,
+static struct i2c_driver nunchuk_driver = {
 	.driver = {
-
-	}
+		.name = "nunchuk",
+		.of_match_table = nunchuk_dt_match,
+	},
+	.probe = nunchuk_probe,
+	.remove = nunchuk_remove
 };
+
 
 module_i2c_driver(nunchuk_driver);
 MODULE_AUTHOR("Marcin Kosma Hyżorek");
